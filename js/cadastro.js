402085c6eb2114663c2bca6db497a1b14c6aa687
Andrew Barbosa Deras <andrewderas7@gmail.com>
2022-11-02 14:42:24 -0300

    Melhorando o JS

---
+++ js/cadastro.js
@@ -0,0 +1,44 @@
+console.log('andrew');
+
+const preencherForm = (endereco) => {
+  document.querySelector('#rua').value = endereco.logradouro;
+  document.querySelector('#bairro').value = endereco.bairro;
+  document.querySelector('#municipio').value = endereco.localidade;
+  document.querySelector('#uf').value = endereco.uf;
+};
+
+const limparForm = (endereco) => {
+  document.querySelector('#rua').value = '';
+  document.querySelector('#bairro').value = '';
+  document.querySelector('#municipio').value = '';
+  document.querySelector('#uf').value = '';
+};
+
+
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length === 8 && eNumero(cep);
+
+
+const pesquisarCep = async () => {
+
+  limparForm();
+
+  const cep = document.querySelector('#cep').value;
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+  if (cepValido(cep)) {
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    if (endereco.hasOwnProperty('erro')) {
+      document.querySelector('#cep').value = 'Por favor, insira um CEP válido.'
+    } else {
+      preencherForm(endereco);
+    }
+  } else {
+    document.querySelector('#cep').value = 'Por favor, insira um CEP válido.'
+  }
+}
+
+document.querySelector('#cep')
+  .addEventListener('focusout', pesquisarCep);
