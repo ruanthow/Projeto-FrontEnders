08cae87b2c12f97d2ef8a2f40927b810beedc98f
wesleyrick <wesleyricardo910@gmail.com>
2022-11-24 09:24:54 -0300

    Padrão de pasta

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Projeto-FrontEnders
-Projeto em grupo focada em vender um produto voltado a tecnologia
+# SpokeBuds para FrontEnders 💻	
+Criação de página para produto inovador e lançamento da empresa FrontEnders.
+
+</div>
+<p align="center">
+<img src="https://ruanthow.github.io/Projeto-FrontEnders/assets/img/logoSpokebudsYellowBlack.png">
+</p>
+
+## O projeto 
+O projeto consiste em ser um fone de ouvido que consegue traduzir qualquer idioma quando for preciso. Ele funcionaria da seguinte forma, quando o seu usuário tivesse vontade de traduzir a fala de outra pessoa ou dela mesma em outro idioma. O usuário ligaria essa função no seu smartphone qualquer através de um aplicativo e o microfone embutido no fone captaria oque foi dito e traduziria para a linguagem escolhida.
+
+
+
+## Objetivo do projeto 📍
+Para esse projeto, montamos uma web page de lançamento para o nosso produto inovador, um fone tradutor de idiomas. 
+
+
+
+## O grupo
+Andrew, Fabricio, Mariana, Ruan, Wesley 
+
+
+## Tecnologias 🌐
+
+🛠️HTML5, CSS3, JavaScript, Bootstrap, Figma and Vscode
+<div style="display: inline_block"><br>
+<img align="center" alt="html" height="30" width="40" src="https://user-images.githubusercontent.com/25181517/117447535-f00a3a00-af3d-11eb-89bf-45aaf56dbaf1.png">
+<img align="center" alt="css" height="30" width="40" src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png">
+<img align="center" alt="figma" height="30" width="40" src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png">
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+<img align="center" alt="vscode" height="30" width="40" src="https://user-images.githubusercontent.com/25181517/182618272-390ab138-7b29-44a0-85a2-62633957d815.png">
+
---
+++ js/cadastro.js
@@ -0,0 +1,125 @@
+console.log('andrew');
+
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+
+const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+const rgRegex = /(^\d{1,2}).?(\d{3}).?(\d{3})-?(\d{1}|X|x$)/;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  nameValidate();
+  emailValidate();
+  firstPasswordValidate();
+  comparePassword();
+  rgValidate();
+  pesquisarCep();
+  showMensagem();
+});
+
+function showMensagem() {
+  const popUp = document.querySelector('.popUp');
+  popUp.style.display = 'block';
+}
+
+function setError(index) {
+  campos[index].style.border = '2px solid red'
+  spans[index].style.display = 'block';
+}
+
+function removeError(index) {
+  campos[index].style.border = '';
+  spans[index].style.display = 'none';
+}
+
+function nameValidate() {
+  if (campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+}
+
+function emailValidate() {
+  if (!emailRegex.test(campos[3].value)) {
+    setError(3);
+  } else {
+    removeError(3);
+  }
+}
+
+function firstPasswordValidate() {
+  if (campos[1].value.length < 8) {
+    setError(1);
+  } else {
+    removeError(1);
+    comparePassword();
+  }
+}
+
+function comparePassword() {
+  if (campos[1].value == campos[4].value && campos[4].value.length >= 8) {
+    removeError(4);
+  } else {
+    setError(4);
+  }
+}
+
+function rgValidate() {
+  if (!rgRegex.test(campos[2].value)) {
+    setError(2);
+  } else {
+    removeError(2);
+  }
+}
+
+const preencherForm = (endereco) => {
+  document.querySelector('#rua').value = endereco.logradouro;
+  document.querySelector('#bairro').value = endereco.bairro;
+  document.querySelector('#municipio').value = endereco.localidade;
+  document.querySelector('#uf').value = endereco.uf;
+};
+
+function limparForm() {
+  const popUp = document.querySelector('.popUp');
+  popUp.style.display = 'none';
+  document.querySelector('#name').value = '';
+  document.querySelector('#email').value = '';
+  document.querySelector('#senha').value = '';
+  document.querySelector('#confirmarSenha').value = '';
+  document.querySelector('#rg').value = '';
+  document.querySelector('#cep').value = '';
+  document.querySelector('#rua').value = '';
+  document.querySelector('#bairro').value = '';
+  document.querySelector('#municipio').value = '';
+  document.querySelector('#uf').value = '';
+};
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length === 8 && eNumero(cep);
+
+const pesquisarCep = async () => {
+
+
+  const cep = document.querySelector('#cep').value;
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+  if (cepValido(cep)) {
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    if (endereco.hasOwnProperty('erro')) {
+      setError(5);
+    } else {
+      removeError(5);
+      preencherForm(endereco);
+    }
+  } else {
+    setError(5);
+  }
+}
+
+document.querySelector('#cep')
+  .addEventListener('focusout', pesquisarCep);
+
+
